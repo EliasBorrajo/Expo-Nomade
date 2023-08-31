979ece07a55e1518586d95da722a0a72a32b3be5
Elias Borrajo <borrajo.elias@gmail.com>
2023-08-31 09:15:24 +0200

    Forms main correction doublon

--- lib/admin_forms/forms.dart
+++ lib/admin_forms/forms.dart
@@ -14,7 +14,7 @@ Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); // TODO : REMOVE
 
   runApp( const adminForms());
 }
