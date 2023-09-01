1cc60586fc5240fcf6fe7fd818ba95d9ab0c7da4
lucadelbuopno <lucadelbuono2000@gmail.com>
2023-09-01 12:49:57 +0200

    feat: Flow from database

--- lib/admin_forms/Migrations/MigrationListPage.dart
+++ lib/admin_forms/Migrations/MigrationListPage.dart
@@ -1,6 +1,8 @@
+import 'package:expo_nomade/admin_forms/dummyData.dart';
 import 'package:expo_nomade/dataModels/Migration.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:latlong2/latlong.dart';
 import 'MigrationDetailsPage.dart';
 
 
@@ -10,52 +12,123 @@ import 'MigrationDetailsPage.dart';
 /// Displays a list of museums.
 /// When a museum is tapped, the [MuseumDetailPage] is displayed.
 ///
-class MigrationListPage extends StatelessWidget {
-  final List<Migration> migrations;
+class MigrationListPage extends StatefulWidget {
+  //final List<Migration> migrations;
   final FirebaseDatabase database;
 
-  const MigrationListPage({super.key, required this.migrations ,  required this.database}); // Constructeur
-/*
-  // void _seedDatabase() async {
-  //   // Get a reference to your Firebase database
-  //   DatabaseReference databaseReference = database.ref();
-  //
-  //   // Loop through the dummyMuseums and add them to the database
-  //   for (var museum in migrations) {
-  //     await databaseReference.child('museums').push().set({
-  //       'name': museum.name,
-  //       'address': {
-  //         'latitude': museum.address.latitude,
-  //         'longitude': museum.address.longitude,
-  //       },
-  //       'website': museum.website,
-  //       // Add other fields as needed
-  //     });
-  //   }
-  // }
-  //
-  // void _readDatabase() async {
-  //   DatabaseReference databaseReference = database.ref().child('museums');
-  //
-  //   DataSnapshot snapshot = await databaseReference.get();
-  //
-  //   if (snapshot.value != null) {
-  //     print('Museum data:');
-  //     Map<dynamic, dynamic> museumsData = snapshot.value as Map<dynamic, dynamic> ;
-  //     museumsData.forEach((key, value) {
-  //       print('Museum ID: $key');
-  //       print('Name: ${value['name']}');
-  //       print('Address:');
-  //       print('  Latitude: ${value['address']['latitude']}');
-  //       print('  Longitude: ${value['address']['longitude']}');
-  //       print('Website: ${value['website']}');
-  //       print('---');
-  //     });
-  //   } else {
-  //     print('No museums found in the database.');
-  //   }
-  // }
-*/
+  const MigrationListPage({super.key, required this.database});
+
+  @override
+  _MigrationListPageState createState() => _MigrationListPageState();
+}
+
+class _MigrationListPageState extends State<MigrationListPage>{
+
+  late List<Migration> migrations = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMigrationsFromFirebaseAndListen();
+  }
+
+  
+  void _loadMigrationsFromFirebaseAndListen() async {
+    DatabaseReference migrationsRef = widget.database.ref().child('migrations');
+    
+    migrationsRef.onValue.listen((DatabaseEvent event) { 
+      if(event.snapshot.value != null){
+        List<Migration> updatedMigrations = [];
+        Map<dynamic, dynamic> migrationsData = event.snapshot.value as Map<dynamic, dynamic>;
+        
+        migrationsData.forEach((key, value) {
+          List<MigrationSource>? polygons = [];
+
+          if(value['polygons'] != null){
+            List<dynamic> polygonsData = value['polygons'] as List<dynamic>;
+
+            for (var polyValue in polygonsData) {
+              List<dynamic> pointsData = polyValue['points'] as List<dynamic>;
+              List<LatLng> points = [];
+
+              for(var point in pointsData){
+                points.add(
+                  LatLng(
+                    point['latitude'] as double,
+                    point['longitude'] as double,
+                  )
+                );
+              }
+
+              MigrationSource source = MigrationSource(
+                  points: points,
+                  //color: polyValue['color']! as Color,
+                  name: polyValue['name']! as String ,
+              );
+
+              polygons.add(source);
+
+              Migration migration = Migration(
+                name: value['name'] as String,
+                description: value['description'] as String,
+                arrival: value['arrival'] as String,
+                polygons: polygons
+              );
+
+              updatedMigrations.add(migration);
+
+              setState(() {
+                migrations = updatedMigrations;
+              });
+
+            }
+          }
+        });
+      }
+    });
+  }
+
+  void _seedDatabase() async {
+    // Get a reference to your Firebase database
+    DatabaseReference databaseReference = widget.database.ref();
+
+    try{
+      for (var migration in dummyMigrations) {
+        Map<String, dynamic> migrationData = {
+          'name': migration.name,
+          'description': migration.description,
+          'arrival': migration.arrival,
+        };
+        if (migration.polygons != null) {
+          migrationData['polygons'] = [];
+          for (var polygon in migration.polygons!) {
+            Map<String, dynamic> polygonData = {
+              'color': polygon.color.toString(),
+              'name': polygon.name,
+            };
+            if (polygon.points != null) {
+              polygonData['points'] = [];
+              for (var point in polygon.points!) {
+                Map<String, double> pointData = {
+                  'latitude': point.latitude.toDouble(),
+                  'longitude': point.longitude.toDouble(),
+                };
+                polygonData['points'].add(pointData);
+              }
+            }
+            migrationData['polygons'].add(polygonData);
+          }
+        }
+        await databaseReference.child('migrations').push().set(migrationData);
+        print('Museum seeded successfully: ${migration.name}');
+      }
+      print('Database seeding completed.');
+    }
+    catch (error) {
+      print('Error seeding database: $error');
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -88,24 +161,17 @@ class MigrationListPage extends StatelessWidget {
               onPressed: () {
                 // ToDo : Naviguer vers la page d'ajout de musée
               },
-              label: Text('Add museum'),
+              label: Text('Add migration'),
               icon: Icon(Icons.add),
-              heroTag: 'add_museum',
+              heroTag: 'add_migration',
             ),
-            SizedBox(height: 10), // Add some spacing between the FABs
-            // FloatingActionButton.extended(
-            //   onPressed: _seedDatabase,
-            //   label: Text('Seed Database'),
-            //   icon: Icon(Icons.cloud_upload),
-            //   heroTag: 'seed_database',
-            // ),
-            // SizedBox(height: 10),
-            // FloatingActionButton.extended(
-            //   onPressed: _readDatabase,  // Utilise la méthode de lecture
-            //   label: Text('Read Database'),  // Libellé du bouton
-            //   icon: Icon(Icons.cloud_download),  // Icône du bouton
-            //   heroTag: 'read_database',  // Tag héros unique
-            // ),
+            const SizedBox(height: 10),
+            FloatingActionButton.extended(
+              onPressed: _seedDatabase,
+              label: Text('Seed Database'),
+              icon: Icon(Icons.cloud_upload),
+              heroTag: 'seed_database',
+            ),
           ],
         ),
       ),
