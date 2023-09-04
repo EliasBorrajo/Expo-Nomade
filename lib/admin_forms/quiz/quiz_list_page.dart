2cb25143d4a8e20231f17c378b93849a236206ca
Milena <milenalonfat@gmail.com>
2023-09-04 15:31:21 +0200

    search in question list

--- lib/admin_forms/quiz/quiz_list_page.dart
+++ lib/admin_forms/quiz/quiz_list_page.dart
@@ -20,6 +20,8 @@ class QuizListPage extends StatefulWidget {
 class _QuizListPageState extends State<QuizListPage> {
   List<Question> questions = [];
   StreamSubscription<DatabaseEvent>? _subscription;
+  final TextEditingController _searchController = TextEditingController();
+  List<Question> filteredQuestions = [];
 
   @override
   void initState() {
@@ -51,6 +53,7 @@ class _QuizListPageState extends State<QuizListPage> {
 
             setState(() {
               questions = fetchedQuestions;
+              filteredQuestions = fetchedQuestions;
             });
           }
         }
@@ -74,6 +77,7 @@ class _QuizListPageState extends State<QuizListPage> {
 
       setState(() {
         questions.removeWhere((question) => question.id == questionId);
+        _searchController.clear();
       });
 
       ScaffoldMessenger.of(context).showSnackBar(
@@ -112,32 +116,75 @@ class _QuizListPageState extends State<QuizListPage> {
     );
   }
 
+  void _clearSearch() {
+    _searchController.clear();
+    setState(() {
+      filteredQuestions = questions;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Liste des questions')),
-      body: questions.isEmpty
-          ? const Center(
+      appBar: AppBar(
+        title: const Text('Liste des questions')
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'Rechercher...',
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: _searchController.text.isNotEmpty
+                    ? ElevatedButton(
+                  onPressed: _clearSearch,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                  ),
+                  child: const Icon(Icons.clear),
+                )
+                    : null,
+              ),
+              onChanged: (value) {
+                setState(() {
+                  filteredQuestions = questions.where((question) {
+                    final query = value.toLowerCase();
+                    return question.questionText.toLowerCase().contains(query);
+                  }).toList();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: filteredQuestions.isEmpty
+                ? const Center(
               child: Text(
                 'Aucune question trouvée dans la base de données.',
               ),
             )
-          : ListView.builder(
-              itemCount: questions.length,
+                : ListView.builder(
+              itemCount: filteredQuestions.length,
               itemBuilder: (context, index) {
                 return Column(
                   children: [
                     QuestionListItem(
-                        question: questions[index],
-                        onDeletePressed: _deleteQuestion,
-                        onEditPressed: _navigateToEditQuestionPage),
-                    const Divider(height: 1), // Ajoute une ligne de séparation
+                      question: filteredQuestions[index],
+                      onDeletePressed: _deleteQuestion,
+                      onEditPressed: _navigateToEditQuestionPage,
+                    ),
+                    const Divider(height: 1),
                   ],
                 );
               },
             ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton.extended(
-        //onPressed: _seedDatabase,
         onPressed: _navigateToAddQuestionPage,
         label: const Text('Ajouter une question'),
         icon: const Icon(Icons.add),
