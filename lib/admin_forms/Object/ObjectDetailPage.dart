517f3696e8cc6b6b88e3e4acd573f6211b5dadf5
lucadelbuopno <lucadelbuono2000@gmail.com>
2023-09-08 12:11:35 +0200

    fix: layout

--- lib/admin_forms/Object/ObjectDetailPage.dart
+++ lib/admin_forms/Object/ObjectDetailPage.dart
@@ -92,9 +92,8 @@ class _ObjectDetailPageState extends State<ObjectDetailPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Détails de l'objet : " + widget.object.name),
+        title: Text("Détails de l'objet : ${widget.object.name}"),
         backgroundColor: Colors.blue,
-        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -105,29 +104,27 @@ class _ObjectDetailPageState extends State<ObjectDetailPage> {
               'Nom',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 4), // Added a little space after the "Nom" label
             Text(
               widget.object.name,
-              style: const TextStyle(fontSize: 16),
+              style: const TextStyle(fontSize: 22),
             ),
-            SizedBox(height: 12), // Space between the object name and its description label
-            Text(
-              'Description:',
+            const SizedBox(height: 16), // Space between the object name and its description label
+            const Text(
+              'Description',
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 8),
             Text(
               widget.object.description,
-              style: TextStyle(
-                fontSize: 16,
+              style: const TextStyle(
+                fontSize: 22,
               ),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Expanded(child: ImageGallery(imageUrls: URLS)),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             FloatingActionButton.extended(
               onPressed: () async {
                 var urlNewImage = await firebaseStorageUtil.uploadImageFromGallery(FirebaseStorageFolder.museums);
@@ -137,8 +134,8 @@ class _ObjectDetailPageState extends State<ObjectDetailPage> {
                   });
                 }
               },
-              label: Text('Ajouter une image'),
-              icon: Icon(Icons.add_rounded),
+              label: const Text('Ajouter une image'),
+              icon: const Icon(Icons.add_rounded),
               heroTag: 'add_museum',
               backgroundColor: Colors.blue,
             )
